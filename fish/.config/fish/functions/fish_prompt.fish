101591ed8fb848995be07475219275b08b10c57e
critical_fernet <vaibhavmagu2006@gmail.com>
2025-07-03 20:50:30 +0530

    Added Custom Theme for firefox and minor tweaks.

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -1,12 +1,3 @@
 function fish_prompt
-set -l last $status
-set -l clr
-if test $status -ne 0
-set clr (set_color red)
-else
-set clr (set_color green)
-end
-echo ''
-echo (set_color blue) (prompt_pwd -D 3 -d 3)
-echo $clr '❱ '
+echo (set_color green) '❱ '
 end
